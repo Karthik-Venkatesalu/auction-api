2dd6893c49e4735bd5bf2e752607f51b3ed3a988
Karthik-Venkatesalu <karthikvenkat40@gmail.com>
2020-11-13 08:44:31 +0530

    renamed the module as per clean architecture

---
+++ api/Infrastructure.Service/Facade.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Service.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class Facade
+    {
+        public List<Document<PropertyCatalog>> GetProperties()
+        {
+            return null;
+        }
+    }
+}
---
+++ api/Infrastructure.Service/Interfaces/IFacade.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Service.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Interfaces
+{
+    public interface IFacade
+    {
+        List<Document<PropertyCatalog>> GetProperties();
+    }
+}
